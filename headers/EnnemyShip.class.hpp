591d63e15c4ee09c8d68bdbbcf20f8b2e33ebca9
Alexandre Petit <alexandre.petit.vilapon@gmail.com>
2015-01-10 16:27:27 +0100

    Added EnnemyShip class

--- headers/EnnemyShip.class.hpp
+++ headers/EnnemyShip.class.hpp
@@ -6,7 +6,29 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/10 15:37:36 by alex              #+#    #+#             */
-/*   Updated: 2015/01/10 15:37:37 by alex             ###   ########.fr       */
+/*   Updated: 2015/01/10 16:26:04 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ENNEMYSHIP_CLASS_HPP
+# define ENNEMYSHIP_CLASS_HPP
+
+# include "AShip.class.hpp"
+# include "Projectile.class.hpp"
+
+class EnnemyShip : public AShip {
+	public:
+		EnnemyShip();
+		EnnemyShip(int x, int y);
+		~EnnemyShip();
+
+		Projectile*	openFire(void);
+
+	protected:
+		static int	_index;
+		int			_id;
+		int			_x;
+		int			_y;
+};
+
+#endif
--- sources/EnnemyShip.class.cpp
+++ sources/EnnemyShip.class.cpp
@@ -6,7 +6,45 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/10 15:37:29 by alex              #+#    #+#             */
-/*   Updated: 2015/01/10 15:37:39 by alex             ###   ########.fr       */
+/*   Updated: 2015/01/10 16:27:07 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "../headers/EnnemyShip.class.hpp"
+
+int		EnnemyShip::_index = 0;
+
+EnnemyShip::EnnemyShip(void) : _id(EnnemyShip::_index), _x(0), _y(0) {
+	if (DebugEntity::getDebug() == true)
+		std::cout << "EnnemyShip #" << this->_id << " created at x=" << this->_x << "; y=" << this->_y << "." << std::endl;
+
+	EnnemyShip::_index++;
+
+	return ;
+}
+
+EnnemyShip::EnnemyShip(int x, int y) : _id(EnnemyShip::_index), _x(x), _y(y) {
+	if (DebugEntity::getDebug() == true)
+		std::cout << "EnnemyShip #" << this->_id << " created at x=" << this->_x << "; y=" << this->_y << "." << std::endl;
+
+	EnnemyShip::_index++;
+
+	return ;
+}
+
+EnnemyShip::~EnnemyShip() {
+	if (DebugEntity::getDebug() == true)
+		std::cout << "EnnemyShip #" << this->_id << " destroyed." << std::endl;
+
+	return ;
+}
+
+Projectile*	EnnemyShip::openFire(void) {
+	Projectile	*projectile = new Projectile(this->_x - 1, this->_y);
+
+	if (DebugEntity::getDebug() == true)
+		std::cout << "EnnemyShip #" << this->_id << " opened fire from x=" << this->_x << "; y=" << this->_y << " creating a projectile at x=" << projectile->getX() << "; y=" << projectile->getY() << "." << std::endl;
+
+	return projectile;
+}
